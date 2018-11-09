64a69e7b11ae4014ea213eae1c2524d46b1130c4
Anatolii_Larionov <Anatolii_Larionov@epam.com>
2018-11-09 15:20:08 +0400

    add_constructor_bus

--- src/main/java/vcs_git/Bus.java
+++ src/main/java/vcs_git/Bus.java
@@ -4,4 +4,12 @@ public class Bus {
 
     private String name;
     private int year;
+
+    public Bus() {
+    }
+
+    public Bus(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
 }
--- src/main/java/vcs_git/MainCars.java
+++ src/main/java/vcs_git/MainCars.java
@@ -1,4 +1,9 @@
 package vcs_git;
 
 public class MainCars {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !!!");
+}
 }
