3df732b238de43a97d0fc8cd2df6fa18ddcffc6a
Anatolii_Larionov <Anatolii_Larionov@epam.com>
2018-11-09 15:22:54 +0400

    add_methods_bus

--- src/main/java/vcs_git/Bus.java
+++ src/main/java/vcs_git/Bus.java
@@ -12,4 +12,20 @@ public class Bus {
         this.name = name;
         this.year = year;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
